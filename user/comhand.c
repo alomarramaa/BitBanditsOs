3fc391942e479deaa6a04bb0022851a01070b042
Rama Al-Omar <alomar.ramaa@gmail.com>
2024-01-29 12:45:10 -0500

    fixes

--- user/comhand.c
+++ user/comhand.c
@@ -1,13 +1,12 @@
 
-#include <stdio.h>
 #include <string.h>
+#include <sys_req.h>
 
 #define READ 1
 #define WRITE 2
 #define COM1 3
 
-// Prints version and compile date
-void printVersion()
+void version(void) // Prints version and compile date
 {
     const char *version = "MPX Version R1";
     const char *compileDate = "Compiled on: ";
@@ -15,81 +14,107 @@ void printVersion()
     sys_req(WRITE, COM1, compileDate, strlen(compileDate));
 }
 
-void printHelp()
+void help(void) // Prints all available commands
 {
-    const char *helpText = "Avaliable Commands: \n"
-                           "1. Shutdown - Shut down the system\n"
-                           "2. Version - Display the current version & compilation date\n"
-                           "3. Help - Display all avaliable commands";
+    const char *helpText = "Available Commands: \n"
+                           "1. Shutdown - Shut down the system"
+                           "2. Version - Display the current version & compilation date"
+                           "3. Help - Display all available commands"
+                           "4. Echo - Repeats previous message"
+                           "5. Get Date - Display current date"
+                           "6. Get Time -  Display current time"
+                           "7. Set Date - Set date to desired month/day/year"
+                           "8. Set Time -  Set time to desired hour/minute/second";
 
     sys_req(WRITE, COM1, helpText, strlen(helpText));
 }
 
-void confirmShutdown()
+int shutdown(void)
 {
+    sys_req(WRITE, COM1, "Are you sure you want to shut down? (y/n)"); // Confirmation to shut down
+
+    char confirm[50] = {0};
+    int nread = sys_req(READ, COM1, confirm, sizeof(confirm));
+
+    if (nread > 0 && confirm[0] == 'y') // shutdown confirmed
     {
-        // Confirmation to shut down
-        sys_req(WRITE, COM1, "Are you sure you want to shut down? (y/n)");
-
-        char confirm[50] = {0};
-        int nread = sys_req(READ, COM1, confirm);
-
-        if (strcmp(confirm), "y" == 0)
-        {
-            sys_req(WRITE, COM1, "Shutdown confirmed.");
-            return;
-        }
-
-        else
-        {
-            sys_req(WRITE, COM1, "Shutdown canceled.\n");
-        }
+        sys_req(WRITE, COM1, "Shutdown confirmed.");
+        return 1;
+    }
+    else // Cancel shutdown
+    {
+        sys_req(WRITE, COM1, "Shutdown canceled.\n");
+        return 0;
     }
 }
 
+
 void comhand(void)
 {
+    /* THE BONUS THING FOR CREATIVE STATUP
+         _________
+        / ======= \
+       / __________\
+      | ___________ |
+      | | -       | |
+      | |  CS450  | |
+      | |_________| |________________________
+      \=____________/   Bit Bandits          )
+      / """"""""""" \                       /
+     / ::::::::::::: \                  =D-'
+    (_________________)
+     */
 
-    // Basic command handler format
-    // Big if/else statement, split/check input, match and run method for command
-    // Delete and arrow keys use ANSI codes
-    // Menu style
+    sys_req(WRITE, COM1, "Comhand Initialized: Please write your preferred command\n", 56);
+    sys_req(WRITE, COM1, "Available commands: \n\techo\n\tget\n\thelp\n\tset\n\tshutdown\n\tversion\n", 70);
+
     for (;;)
     {
         char buf[100] = {0};
         int nread = sys_req(READ, COM1, buf, sizeof(buf));
         sys_req(WRITE, COM1, buf, nread);
 
-        // Shutdown Command
-        if (strcmp(buf, "shutdown") == 0)
+        if (strcmp(buf, "shutdown") == 0) // Shutdown Command
         {
-            // Needs to exit the command handler loop
-            // Execution will return to kmain() & system will halt
-            // Must ask for confirmation
-
             sys_req(WRITE, COM1, "Shutdown confirmed.");
-            confirmShutdown();
-            return 0;
+            if (shutdown() == 1)
+            {
+                break; // exit the loop if shutdown is confirmed
+            }
         }
-        // Version Command
-        //  Prints current version of MPX & compilation date (in this case, R1)
 
-        else if (strcmp(buf, "version" == 0))
+        if (strcmp(buf, "version") == 0) // Version Command
         {
-            printVersion();
+            version();
         }
 
-        // Help Command
-        //  Provides usage instructions for all commands
-        //  Strings need to be hardcoded
-
-        else if (strcmp(buf, "help") == 0)
+        else if (strcmp(buf, "help") == 0) // Help Command
         {
-            printHelp();
+            help();
         }
+        // Add other commands here if needed
 
-        // Get/Set Date Command
+        /*
 
-        // Get/Set Time Command
+                else if (strcmp(buf, "get date") == 0) // Get Date Command
+                {
+                    getDate();
+                }
+
+                else if (strcmp(buf, "set date") == 0) // Set Date Command
+                {
+                    setDate();
+                }
+
+                else if (strcmp(buf, "get time") == 0) // Set Date Command
+                {
+                    getTime();
+                }
+
+                else if (strcmp(buf, "set time") == 0) // Set Date Command
+                {
+                    setTime();
+                }
+                */
     }
 }
