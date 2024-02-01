6235d4f78ab4d206f0f16639e33b4dda592d4f11
Connor <ceg0026@mix.wvu.edu>
2024-01-31 19:39:39 -0500

    Implemented time

---
+++ include/user/time.h
@@ -0,0 +1,9 @@
+// @brief Displays teh system time
+void get_time();
+
+/// @brief Changes teh system time to a time inputed by the user witht hegiven parameters
+/// @param hours 
+/// @param minutes 
+/// @param seconds 
+void set_time(int hours, int minutes, int seconds);
+
