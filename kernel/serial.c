17480775f0bd508db89a88b7bc160a98f8810679
ItGarrett <gpb00001@mix.wvu.edu>
2024-02-01 18:10:27 -0500

    Working on backspace

--- kernel/serial.c
+++ kernel/serial.c
@@ -140,9 +140,9 @@ int serial_poll(device dev, char *buffer, size_t len)
 					buffer[index--] = '\0';
 					bufferCount--;
 					tempIndex--;
-					outb(dev, 8);
+					outb(dev, '\0');
 					outb(dev, ' ');
-					outb(dev, 8);
+					outb(dev, '\0');
 					break;
 				}
 
