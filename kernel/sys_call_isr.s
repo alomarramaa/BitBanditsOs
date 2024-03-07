e88926fb9e03805a24c3c912e2c921559c523818
ItGarrett <gpb00001@mix.wvu.edu>
2024-03-07 16:56:55 -0500

    Testing prev commands

--- kernel/sys_call_isr.s
+++ kernel/sys_call_isr.s
@@ -1,33 +1,33 @@
-;bits 32
-;global sys_call_isr
-;
-;;;; System call interrupt handler. To be implemented in Module R3.
-;extern sys_call			; The C function that sys_call_isr will call
-;
-;sys_call_isr:
-;	; Save context
-;    push ad             ; Push all general purpose registers
-;    push gs             ; Push remaining segment registers
-;    push fs
-;    push es
-;    push ss
-;    push ds
-;
-;    push dword esp      ; Push ESP as a parameter for the C function call
-;
-;    call sys_call      ; Call your C function
-;
-;	; Set ESP based on the return value of your function (EAX)
-;	mov esp, eax
-;
-;	; Restore context
-;    add esp, 4          ; Adjust ESP to remove the parameter pushed earlier
-;
-;    pop ds              ; Pop segment registers
-;    pop ss
-;    pop es
-;    pop fs
-;    pop gs
-;    pop ad               ; Pop all general purpose registers
-;
-;	iret ; Return from ISR
+bits 32
+global sys_call_isr
+
+;;; System call interrupt handler. To be implemented in Module R3.
+extern sys_call			; The C function that sys_call_isr will call
+
+sys_call_isr:
+	; Save context
+    push ad             ; Push all general purpose registers
+    push gs             ; Push remaining segment registers
+    push fs
+    push es
+    push ss
+    push ds
+
+    push dword esp      ; Push ESP as a parameter for the C function call
+
+    call sys_call      ; Call your C function
+
+	; Set ESP based on the return value of your function (EAX)
+	mov esp, eax
+
+	; Restore context
+    add esp, 4          ; Adjust ESP to remove the parameter pushed earlier
+
+    pop ds              ; Pop segment registers
+    pop ss
+    pop es
+    pop fs
+    pop gs
+    pop ad               ; Pop all general purpose registers
+
+	iret ; Return from ISR
