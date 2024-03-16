72b6ef479531fa525f7b1e20883353a8127b2a84
ItGarrett <gpb00001@mix.wvu.edu>
2024-03-16 19:41:18 -0400

    Testing R3

--- kernel/sys_call_isr.s
+++ kernel/sys_call_isr.s
@@ -1,37 +1,31 @@
 bits 32
 global sys_call_isr
 
-;;; System call interrupt handler. To be implemented in Module R3.
-extern sys_call			; The C function that sys_call_isr will call
+extern sys_call            ; Declare sys_call as an external symbol
 
 sys_call_isr:
-	; Save context
-    pusha             ; Push all general purpose registers
-
-    push gs             ; Push remaining segment registers
+    ; Save context
+    pusha                   ; Push all general purpose registers
+    push gs                 ; Push remaining segment registers
     push fs
     push es
     push ss
     push ds
 
-    push dword esp      ; Push ESP as a parameter for the C function call
+    push dword esp          ; Push ESP as a parameter for the C function call
 
-    mov eax, dword sys_call
+    call sys_call           ; Call the C function
 
-    call eax      ; Call your C function
+    ; Set ESP based on the return value of your function (EAX)
+    mov esp, eax
 
-	; Set ESP based on the return value of your function (EAX)
-	mov esp, eax
-
-	; Restore context
-    add esp, 4          ; Adjust ESP to remove the parameter pushed earlier
-
-    pop ds              ; Pop segment registers
+    ; Restore context
+    pop ds                  ; Pop segment registers
     pop ss
     pop es
     pop fs
     pop gs
 
-    popa               ; Pop all general purpose registers
+    popa                    ; Pop all general purpose registers
 
-	iret ; Return from ISR
+    iret                    ; Return from ISR
