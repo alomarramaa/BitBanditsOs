dc443de663fd37b8b3428cb94eec11f9493695bf
Malone Ingham <mji00001@mix.wvu.edu>
2024-04-03 20:57:21 -0400

    user_commands modified

--- include/user/r5_user_commands.h
+++ include/user/r5_user_commands.h
@@ -1,12 +1,14 @@
 #ifndef _R5_USER_COMMANDS_H
 #define _R5_USER_COMMANDS_H
 
-
 #include <string.h>
 
-void allocateMemory(void);
-void freeMemory(void);
-void showAllocatedMemory(void);
-void showFreeMemory(void);
+void allocateMemory(HeapManager *heap_manager, size_t size);
+
+void freeMemory(HeapManager *heap_manager, void *address);
+
+void showAllocatedMemory(HeapManager *heap_manager);
+
+void showFreeMemory(HeapManager *heap_manager);
 
 #endif
